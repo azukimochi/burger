88608706d50e77ad3ac77af0c47c69dd28659e71
Durian <karen.kua7@gmail.com>
2018-09-05 21:06:06 -0400

    Updated css

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,41 +1,42 @@
 var ormFunctions = require("../models/burger.js");
-var orm = require("../config/orm.js");
+// var orm = require("../config/orm.js");
 var express = require("express");
-var app = express();
-app.use(express.static('public'));
 
-module.exports = function (app) {
+// var Router = require("router");
+// var app = express();
+// app.use(express.static('public'));
 
-    app.get("/", function(req, res) {
-        orm.selectAll("burgers", function(result) {
+
+var router = express.Router();
+    router.get("/", function(req, res) {
+        ormFunctions.selectAll(function(result) {
             var data = result;
             res.render("index", {burgers: data});
         });
     });
 
-    app.get("*", function(req, res) {
-        orm.selectAll("burgers", function(result) {
+    router.get("*", function(req, res) {
+        ormFunctions.selectAll(function(result) {
             var data = result;
             res.render("index", {burgers: data});
         });
     });
 
-    app.post("/burgers", function(req, res) {
-        orm.insertOne("burgers", "burger_name", "devoured", req.body.burger_name, false, function(result) {
+    router.post("/burgers", function(req, res) {
+        ormFunctions.insertOne("burger_name", "devoured", req.body.burger_name, false, function(result) {
             console.log("A new burger has been added!");
             res.status(200).end();
         });
     });
 
-    app.put("/devour", function(req, res) {
-        orm.updateOne("burgers", "devoured", true, "id", req.body.id, function(result) {
+    router.put("/devour", function(req, res) {
+        ormFunctions.updateOne("devoured", true, "id", req.body.id, function(result) {
             console.log(`The burger with the ID of ${req.body.id} has been devoured!`);
             res.status(200).end();
         });
     });
 
-
-};
+module.exports = router;
 
 
 // var queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
--- server.js
+++ server.js
@@ -10,11 +10,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-require("./controllers/burgers_controller.js")(app);
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// require("./controllers/burgers_controller.js")(app);
 
 app.listen(PORT, function () {
     console.log("Server listening on: http://localhost:" + PORT);
